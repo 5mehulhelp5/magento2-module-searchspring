177ffe095d425582347fa0a449fcb55240821500
Nilesh D Babar <nilesh.babar@searchspring.com>
2025-01-28 15:10:43 +0530

    Feed format - Checked feed format based on the presigned url. - Format selected based on the url. - File Compression added based on the gz extension for json.gz and csv.gz from the presigned url. - Delimiter support added in category , default set to '>'. - json data added as objects in the file for processing.

--- Api/MetadataInterface.php
+++ Api/MetadataInterface.php
@@ -32,5 +32,4 @@ interface MetadataInterface
     const FORMAT_GZ = 'gz';
     const FORMAT_JSON_GZ = 'json.gz';
     const FORMAT_CSV_GZ = 'csv.gz';
-    const FORMAT_UNKNOWN = 'presigned url file extension supported is csv,json,json.gz,csv.gz';
 }
